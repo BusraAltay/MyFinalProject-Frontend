749cc69fbed3db5280ab071baf0124d6f0003d14
BusraAltay <busraorenaltay@gmail.com>
2021-04-19 10:11:16 +0300

    19.04.2021

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -39,6 +39,7 @@ export class ProductAddComponent implements OnInit {
           for (let i = 0; i < responseError.error.Errors.length; i++) {
             this.toastrService.error(responseError.error.Errors[i].ErrorMessage,"Doğrulama hatası")
           }
+          
         }
       })
     }else{
